7f00fb244681e2e821711f0d9ad02a59a2b4bd18
Szarnecki Przemysław <siarniuk@gmail.com>
2020-12-12 10:26:09 +0100

    add active to correct article

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 });*/
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log('event:', event);
@@ -18,7 +19,7 @@ const titleClickHandler = function(event){
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
 
     
     console.log('clickedElement (with plus): ' + clickedElement);
@@ -33,11 +34,20 @@ const titleClickHandler = function(event){
         activeArticle.classList.remove('active');
     }
     
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector:', articleSelector);
     
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log('targetArticle:', targetArticle);
     
-    /* add class 'active' to the correct article */
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add('active');
+
 }
   
   const links = document.querySelectorAll('.titles a');
